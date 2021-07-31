d095d9dbbd4ad0cb7926f777c78f27d77d909b18
Lemon <lemon@dc7342.im>
2021-07-31 15:29:18 +0500

    add TODO

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-"""
-This is a echo bot.
-It echoes any incoming text messages.
-"""
-
 import logging
 import config
 import requests
@@ -19,23 +14,26 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-ENDPOINT = 'https://habr.com'
+ENDPOINT = 'https://habr.com' # TODO: replace to url from @gim6626
 
+# Keyboard for answer
 button_yes = InlineKeyboardButton(text='Да!', callback_data='yes')
 button_no = InlineKeyboardButton(text='Нет!', callback_data='no')
 keyboard = InlineKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=2)
 keyboard.insert(button_yes)
 keyboard.insert(button_no)
 
+# Keyboard after answer
 empty_keyboard = InlineKeyboardMarkup()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm EchoBot!")
+    await message.reply("Hi!\nI'm FossNews Bot!\n\nSend /url to get Link")
 
 @dp.message_handler(commands=['url'])
 async def echo(message: types.Message):
     link = 'https://habr.com'
+    # link = requests.get(ENDPOINT)
 
     message_text = f'Link: {link}\n\nВключаем в дайджест?'
 
@@ -46,10 +44,12 @@ async def answer(callback: types.CallbackQuery):
     if callback.data == 'yes':
         await callback.answer(text='Спасибо!')
         await callback.message.edit_reply_markup(reply_markup=empty_keyboard)
+        # TODO: add action to FNGS
 
     elif callback.data == 'no':
         await callback.answer(text='Cпасибо')
         await callback.message.edit_reply_markup(reply_markup=empty_keyboard)
+        # TODO: add action to FNGS
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
