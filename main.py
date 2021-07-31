2bebea41ce262d597cc332c52f7ab59dded15ef9
Lemon <lemon@dc7342.im>
2021-07-31 15:22:01 +0500

    inline keyboard hide after answer

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ It echoes any incoming text messages.
 
 import logging
 import config
+import requests
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+    InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -18,7 +19,15 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-links = ['url1','url2','url3']
+ENDPOINT = 'https://habr.com'
+
+button_yes = InlineKeyboardButton(text='Да!', callback_data='yes')
+button_no = InlineKeyboardButton(text='Нет!', callback_data='no')
+keyboard = InlineKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, row_width=2)
+keyboard.insert(button_yes)
+keyboard.insert(button_no)
+
+empty_keyboard = InlineKeyboardMarkup()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -26,13 +35,21 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler(commands=['url'])
 async def echo(message: types.Message):
-    button_yes = KeyboardButton('Да!')
-    button_no = KeyboardButton('Нет!')
-    keyboard = ReplyKeyboardMarkup()
-    keyboard.add(button_yes)
-    keyboard.add(button_no)
+    link = 'https://habr.com'
 
-    await message.answer(message.text, reply_markup=keyboard)
+    message_text = f'Link: {link}\n\nВключаем в дайджест?'
+
+    await message.answer(message_text, reply_markup=keyboard)
+
+@dp.callback_query_handler()
+async def answer(callback: types.CallbackQuery):
+    if callback.data == 'yes':
+        await callback.answer(text='Спасибо!')
+        await callback.message.edit_reply_markup(reply_markup=empty_keyboard)
+
+    elif callback.data == 'no':
+        await callback.answer(text='Cпасибо')
+        await callback.message.edit_reply_markup(reply_markup=empty_keyboard)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
